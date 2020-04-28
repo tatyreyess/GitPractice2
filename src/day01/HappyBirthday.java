0219d6a5bb2fd736db547d77f872ca3f3f7080f8
tatyreyesss <katherinereyes_07@yahoo.com>
2020-04-28 15:36:54 -0400

    add HappyBirthday.java.

---
+++ src/day01/HappyBirthday.java
@@ -0,0 +1,26 @@
+package day01;
+
+public class HappyBirthday {
+    public static void main(String[] args) {
+
+        /* Happy Mothers Day Party!
+
+        Kathy - cupcakes and cake
+        Cezar - chimol
+        Ricardo - rice
+        Vanessa - meat
+        Fatima - cooker
+
+        lol ....
+
+
+
+
+
+         */
+
+
+
+
+    }
+}
